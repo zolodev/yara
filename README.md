ea32f41bcc5a6956bd28d07663b0bdbfb29fe3d8
Zolo <zolo@unicornab.se>
2022-08-22 14:37:11 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# yara
-Yet Another Rougelike Adventure game
+# Yet Another Rougelike Adventure - YARA
+Yet Another Rougelike Adventure game.
+
+To build the docs.
+```sh
+# path: ./docs/
+$ mdbook build
+```
+
+To preview the docs.
+```sh
+# path: ./docs/
+$ mdbook serve --open
+```
---
+++ yara-game/src/main.rs
@@ -0,0 +1,12 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Mon Aug 22 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : This is the start file for YARA
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {
+    todo!()
+}
